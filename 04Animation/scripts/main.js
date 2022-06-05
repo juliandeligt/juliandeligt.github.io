d71a235cee3c4271f9672b28e56d85cdab57165c
juliandeligt <82270985+juliandeligt@users.noreply.github.com>
2022-06-05 19:44:16 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -5,7 +5,7 @@
  var myOtherBox = document.getElementById('myOtherBox');
 
  function spin(){
- myOtherBox.object3D.rotation.x -= rotationSpeed;
+ myOtherBox.object3D.rotation.x -=1 rotationSpeed;
  	console.log(myOtherBox.object3D.rotation.x);
  }
 
